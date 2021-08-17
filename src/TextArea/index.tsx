d8333b3d54de57ab95f978827ca0e37adc198cbb
Sergio L. Benítez D <sergio.kun21@gmail.com>
2021-08-16 22:09:41 -0500

    ETOAST-128: Checking icon scenario

--- src/TextArea/index.tsx
+++ src/TextArea/index.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import getClassName from 'getclassname';
 
 export interface TextAreaProps {
-
+  hasIcon?: boolean;
+  icon?: string
   id?: string;
   isDisabled:boolean;
+  label?: string;
   name?: string;
-  hasIcon?: boolean;
 }
 
 const Padded = ({ children }) => <div style={{margin: "16px"}}> {children} </div>;
 
 const TextArea = (
   { 
-    id = "id",
-    name = "normal",
-    isDisabled = false,
     hasIcon = false,
+    icon = "calendar",
+    id = "id",
+    isDisabled = false,
+    label = "Placeholder",
+    name = "normal",
   }
 : TextAreaProps) => {
   const clTextArea = getClassName({
@@ -24,9 +27,13 @@ const TextArea = (
     "&--has-icon": hasIcon,
   });
 
+  const clIcon = getClassName({
+    base: "eds-icon",
+  });
+
   const clTextAreaIcon = getClassName({
-    base: "eds-icon"
-  })
+    base: "eds-outline-textarea__icon",
+  });
 
   return (
     <section>
@@ -36,11 +43,11 @@ const TextArea = (
             className={clTextArea}
             disabled={isDisabled}
             name={id || name}
+            placeholder={label}
           />
-          <span className={`${clTextAreaIcon}`}></span>
+          <span className={`${clIcon} ${clTextAreaIcon} ${icon}`}></span>
         </div>
       </Padded>
-      TextArea
     </section>
   )
 }
--- stories/TextArea.stories.tsx
+++ stories/TextArea.stories.tsx
@@ -1,20 +1,30 @@
 import { Meta, Story } from '@storybook/react'
 import TextArea, { TextAreaProps } from '../src/TextArea'
-import { noControl, textControl } from './shared/utils'
+import { booleanControl, noControl, textControl } from './shared/utils'
 
 export default {
   title: "@e-toast/TextArea",
   component: TextArea,
   argTypes: {
+    hasIcon: booleanControl(),
+    icon: textControl(),
     id: textControl(),
+    isDisabled: booleanControl(),
     label: textControl()
   }
 } as Meta;
 
 const Template: Story<TextAreaProps> = (args) => <TextArea {...args}/>
 
-export const Basic = Template.bind({});
-Basic.args = {};
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const NormalWithIcon = Template.bind({});
+NormalWithIcon.args = {hasIcon: true, icon: "calendar"};
 
 export const Disabled = Template.bind({});
 Disabled.args = {isDisabled: true};
+
+export const DisabledWithIcon = Template.bind({});
+DisabledWithIcon.args = {hasIcon: true, icon: "calendar", isDisabled: true};
+
