60c08e9cd0103bb85b6c446ce442e8389a0b31a2
John Angel <j_angeldiaz@hotmail.com>
2021-10-01 14:09:24 -0500

    E-TOAST-130: PasswordField component (#8)
    E-TOAST-130: PasswordField component

---
+++ stories/PasswordField.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, Story } from '@storybook/react'
+import PasswordField, { PasswordFieldProps } from '../src/PasswordField'
+import { Icons } from './shared/IconOptions'
+import { 
+  noControl,
+  textControl,
+  objectControl,
+  booleanControl,
+  selectWithOptional,
+} from './shared/utils'
+
+export default {
+  title: "@e-toast/PasswordField",
+  component: PasswordField,
+  argTypes: {
+    id: textControl(),
+    label: textControl(),
+    name: textControl(),
+    icon: selectWithOptional(Icons),
+    error: textControl(),
+    success: booleanControl(),
+    disabled: booleanControl(),
+    inputProps: objectControl(),
+    placeholder: textControl(),
+    value: textControl(),
+    initialValue: textControl(),
+    onChange: noControl(),
+    onIconClick: noControl()
+  }
+} as Meta
+
+const Template: Story<PasswordFieldProps> = (args) => <PasswordField {...args}/>
+
+export const Controlled = Template.bind({})
+Controlled.args = {
+  value: "A password..."
+}
+
+export const Uncontrolled = Template.bind({})
+Uncontrolled.args = {}
